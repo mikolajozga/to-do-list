bda2e7bcb041e9f79c3f08bacd2c4da162f07272
Mikolaj Ozga <mikolaj.ozga03@gmail.com>
2023-05-09 23:12:51 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,86 @@
+{
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+                render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+        
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+               removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+               toggleTaskDone(index);
+            });
+        });
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+
+        for (const task of tasks) {
+            htmlString += `
+                    <li
+                        class="list__item${task.done ? " list__item--done" : ""}"
+                    > 
+                        <button class="js-done checkTaskButton">
+                        ${task.done ? "✔" : ""}
+                        </button>
+                        ${task.content}
+                        <button class="js-remove removeTaskButton"><i class="fa fa-trash"></i></button>
+                    </li>
+                `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
+    };
+
+    
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if(newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
